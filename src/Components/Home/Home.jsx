c9e7c214be2d23c8d12a87a97294cfb8394e500b
Sanskar Pandey <sanskarpandey281@gmail.com>
2023-07-27 12:20:06 +0530

    Initial Commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <button className='dash-btn'>Admin</button>
+            <button className='dash-btn'>Client</button>
+            <button className='bk-btn'> <span className="arrow"></span> <div>Return</div></button>
+        </div>
+    )
+}
+
+export default Home
